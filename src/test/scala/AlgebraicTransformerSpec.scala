7d5f25bb23d8d52dcd0bb485119c7ad1cf0c8ff6
Scott Plummer <Scott.Plummer18@gmail.com>
2018-12-03 09:21:31 -0800

    Remove unused main file and refactor AlgebraicTransformerSpec test cases

--- src/test/scala/AlgebraicTransformerSpec.scala
+++ src/test/scala/AlgebraicTransformerSpec.scala
@@ -1,14 +1,31 @@
 import org.scalatest.FlatSpec
 
+/**
+  * Test class to test different cases for transforming BooleanExpressions into disjunctive normal form
+  */
 class AlgebraicTransformerSpec extends FlatSpec{
-    "A AlgebraicTransformer" should "be able to convert any algebraic expression into DNF form" in {
+    "A AlgebraicTransformer" should "be able to convert simple And and Or expressions into DNF form" in {
         val transformer = new AlgebraicTransformer
 
-        var andExp = And(Variable("A"), Variable("B"))
+        val andExp = And(Variable("A"), Variable("B"))
         assert(transformer.DNFTransform(andExp) === And(Variable("A"), Variable("B")))
 
         val orExp = Or(Variable("A"), Variable("B"))
         assert(transformer.DNFTransform(orExp) === Or(Variable("A"), Variable("B")))
+
+    }
+
+    it should "be able to simplify complex algebraic expression into DNF form" in {
+        val transformer = new AlgebraicTransformer
+
+
+        val complexExpression = Not(And(And(Variable("A"), Variable("B")), Or(Variable("A"), Variable("B"))))
+        assert(transformer.DNFTransform(complexExpression) ===
+            Or(Or(Not(Variable("A")), Not(Variable("B"))), And(Not(Variable("A")), Not(Variable("B")))))
+    }
+
+    it should "simplify Not expressions" in {
+        val transformer = new AlgebraicTransformer
 
         val negativeExp = Not(Variable("A"))
         assert(transformer.DNFTransform(negativeExp) === Not(Variable("A")))
@@ -16,16 +33,9 @@ class AlgebraicTransformerSpec extends FlatSpec{
         val doubleNegationExp = Not(Not(Variable("A")))
         assert(transformer.DNFTransform(doubleNegationExp) === Variable("A"))
 
-        val nestedAndExp = And(And(And(True, True), And(False, False)), And(True, True))
-        assert(transformer.DNFTransform(nestedAndExp) ===
-            And(And(And(True, True), And(False, False)), And(True, True)))
-
-        val complexExpression = Not(And(And(Variable("A"), Variable("B")), Or(Variable("A"), Variable("B"))))
-        assert(transformer.DNFTransform(complexExpression) ===
-            Or(Or(Not(Variable("A")), Not(Variable("B"))), And(Not(Variable("A")), Not(Variable("B")))))
-
         val simplifyNegationsExp = And(Not(True), Not(False))
         assert(transformer.DNFTransform(simplifyNegationsExp) === And(False, True))
+
 
         val rootNotExpression = Not(Or(Or(Variable("A"), Variable("B")), Variable("A")))
         assert(transformer.DNFTransform(rootNotExpression) == And(And(Not(Variable("A")), Not(Variable("B"))), Not
